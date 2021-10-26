6e24b4392d10a6ad4a6224b05599aa4199d01362
JoJ3o <42371079+JoJ3o@users.noreply.github.com>
2021-10-26 12:45:16 +0200

    Successfully get credentials token

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const express = require('express');
 const path = require('path');
 
+const youtubeService = require('./youtubeService');
+
+const server = express();
+
+server.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, '/index.html'))
+);
+
+server.get('/auth', (req, res) => {
+    youtubeService.getCode(res);
+  }
+);
+
+server.get('/callback', (req, res) => {
+    const {code} = req.query;
+    youtubeService.getTokensWithCode(code);
+    res.redirect('/');
+  }
+);
+
+server.listen(3000, () => console.log('Server is ready!'));
---
+++ youtubeService.js
@@ -0,0 +1,79 @@
+const { google } = require('googleapis');
+const util = require('util');
+const fs = require('fs')
+
+const writeFilePromise = util.promisify(fs.writeFile);
+const readFilePromise = util.promisify(fs.readFile);
+
+let liveChatID;
+let nextPageToken;
+const intervalTime = 5000;
+let interval;
+let chatMessages = [];
+let raffleUsersEntered = [];
+
+const save = async(path, data) => {
+  await writeFilePromise(path, data);
+  console.log('Succesfully saved');
+};
+
+const read = async path => {
+  const fileContents = await readFilePromise(path);
+  return JSON.parse(fileContents);
+};
+
+const youtube = google.youtube('v3');
+
+const Oauth2 = google.auth.OAuth2;
+
+const clientID = "702193958045-mog7ismh8cv9j9gipkjkrj6h0rhkf6rh.apps.googleusercontent.com";
+const clientSecret = "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";
+const redirectURI = "http://localhost:3000/callback";
+
+const scope = [
+  'https://www.googleapis.com/auth/youtube.readonly'
+];
+
+const auth = new Oauth2(clientID, clientSecret, redirectURI);
+
+const youtubeService = {};
+
+youtubeService.getCode = response => {
+  const authUrl = auth.generateAuthUrl({
+    access_type: 'offline',
+    scope
+  })
+  response.redirect(authUrl);
+};
+
+youtubeService.getTokensWithCode = async code => {
+  const credentials = await auth.getToken(code);
+  youtubeService.authorize(credentials);
+};
+
+youtubeService.authorize = ({tokens}) => {
+  auth.setCredentials(tokens);
+  console.log('Succesfully set credentials');
+  console.log('tokens', tokens)
+  save('./tokens.json', JSON.stringify(tokens));
+};
+
+auth.on('tokens', () => {
+  console.log('New tokens received');
+  save('./tokens.json', JSON.stringify(tokens));
+});
+
+const checkTokens = async () => {
+  const tokens = await read('./tokens.json');
+  if (tokens) {
+    console.log('Setting tokens');
+    return auth.setCredentials(tokens);
+  }
+  console.log('No tokens found');
+}
+
+checkTokens();
+
+console.log("hi");
+
+module.exports = youtubeService;
